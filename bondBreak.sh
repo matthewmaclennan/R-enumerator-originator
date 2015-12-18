ea3f36902a8ddbc3c2d58c5ba608dd00895edd3b
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-18 12:55:56 -0800

    Update bondBreak.sh

--- bondBreak.sh
+++ bondBreak.sh
@@ -4,9 +4,16 @@
 #Canonicalize SMILES and output fragments separately in cmd.exe openbabel; write to file
 #Reload new file into R
 sn<-AllOriginatorsList(smiles)
-write.table(paste0(unlist(sn),collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
-indirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.smi"
+write.table(paste0(unlist(tn),collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
+indirfile<-"C:/Users/Matthew S. MacLennan/Documents/tn.smi"
 outdirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.out2"
 #substituting indirfile with the path as character string returns error
-system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc")
+#system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -h")
+system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -xh -h")
 new<-as.matrix(read.table(outdirfile))
+
+
+#add explicit H to smiles
+write.table(paste0(smiles,collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
+system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -xh -h")
+smiles<-read.table(outdirfile)
