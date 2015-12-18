33452050a16e9c5bdea436123e8abe66e3d978ce
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-18 13:54:01 -0800

    Create AOLcml.R

---
+++ AOLcml.R
@@ -0,0 +1,12 @@
+#analogous to AllOriginatorsList but breaks bonds through *.cml format
+#convert smiles to cml through system() command
+#break single bond, convert to smiles
+#convert db2sb, convert to smiles
+#convert tb2db, convert to smiles
+#output fragments separately
+#save all appended
+AOLcml.R<-function(smiles){
+#smiles to cml with 3d coords, explicit H
+
+
+}
