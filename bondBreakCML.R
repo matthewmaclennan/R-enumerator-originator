c8042a05bbba827188701ed7cf66658df82849b3
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-18 13:47:32 -0800

    Update bondBreakCML.R

--- bondBreakCML.R
+++ bondBreakCML.R
@@ -1,1 +1,17 @@
+bondbreak<-function(smiles,generations){
+list1<-list()
+  for(i in 1:generations){
+    smiles<-AllOriginatorsList(unlist(smiles))
+    list1[[i]]<-smiles
+  }
+list1
+write.table(paste0(unlist(list1),collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
+indirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.smi"
+outdirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.out2"
+#substituting indirfile with the path as character string returns error
+#system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -h")
+system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -xh -h")
+new<-read.table(outdirfile)
+
+}
 
