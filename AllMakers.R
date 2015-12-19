cca96a8389c864de5972021feac97f55b4b70181
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-19 12:29:03 -0800

    Create AllMakers.R

---
+++ AllMakers.R
@@ -0,0 +1,22 @@
+AllMakers<-function(){
+
+#Find non-Hydrogen atoms
+#List ids for these atoms
+#List already existing single bond connections
+#Number bonds?
+#create string for new conections
+
+
+
+
+#enumerate double bonds
+
+
+
+
+#enumerate triple bonds
+
+
+
+
+}
