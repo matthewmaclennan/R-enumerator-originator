a156aa7234c881d028e301ab3e814d60cbff4044
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-18 16:11:55 -0800

    Update AOLCLoop.R

--- AOLCLoop.R
+++ AOLCLoop.R
@@ -3,7 +3,8 @@
 AOLCLoop<-function(smiles,generations){
 list1<-list()
 for(i in 1:generations){
-  list[[i]]<-AOLcml.R(unlist(smiles))
+  smiles<-AOLcml.R(unlist(smiles))
+  list1[[i]]<-smiles
 }
-
+list1
 }
