d5da11237f1cd70adc38aeb43b6b6e3996d47590
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-19 12:55:25 -0800

    Update AllMakers.R

--- AllMakers.R
+++ AllMakers.R
@@ -1,8 +1,17 @@
 AllMakers<-function(){
 
 #Find non-Hydrogen atoms
+nonH<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("elementType=(?!\\\"H\\\")",
+    readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt"),perl=T)]
 #List ids for these atoms
+unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))
+#List all combinations of bonds
+combos<-as.matrix(expand.grid(unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2])),
+    unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))))
 #List already existing single bond connections
+bonds<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("bond atomRefs",
+    readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt"),perl=T)]
+bondlist<-unlist(lapply(strsplit(bonds,"\\\""),function(x) x[2]))
 #Number bonds?
 #create string for new conections
 
