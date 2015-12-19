2b0d310a17f4a6bf1baa8a4c0f7a4eb61ff3b7a2
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-19 15:44:09 -0800

    Update AllMakers.R

--- AllMakers.R
+++ AllMakers.R
@@ -16,9 +16,25 @@ bondlist<-unlist(lapply(strsplit(bonds,"\\\""),function(x) x[2]))
 bondlistidmat<-matrix(unlist(strsplit(bondlist," ")),ncol=2,byrow=T)
 nonHatomId<-unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))
 nonHatomIdBondList<-bondlistidmat[bondlistidmat[,1] %in% nonHatomId & bondlistidmat[,2] %in% nonHatomId,]
+nhaidblm<-apply(expand.grid(nonHatomId,nonHatomId),1,function(x) paste0(x[1]," ",x[2]))
+###############
+newbonds<-c()
+for(i in 1:length(nonHatomId)){
+	for(j in 1:length(nonHatomId)){
 
+		if(j>i){
+			newbonds<-c(newbonds,paste0(nonHatomId[i]," ",nonHatomId[j]))
+		} else {is.null(j)}
+	
+		newbonds
+	}
+newbonds
 
-#create string for new conections
+}
+
+inserts<-paste0("<bond atomRefs2=\"",newbonds,"\"order=\"1\"/>")
+#
+
 
 
 
