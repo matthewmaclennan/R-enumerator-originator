8aef6f293f89dcf79fc7db166a07435e27c39999
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-19 15:17:45 -0800

    Update AllMakers.R

--- AllMakers.R
+++ AllMakers.R
@@ -1,7 +1,7 @@
 AllMakers<-function(){
 
 #Find non-Hydrogen atoms
-nonH<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("elementType=(?!\\\"H\\\")",
+nonHatom<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("elementType=(?!\\\"H\\\")",
     readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt"),perl=T)]
 #List ids for these atoms
 unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))
@@ -9,10 +9,15 @@ unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))
 combos<-as.matrix(expand.grid(unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2])),
     unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))))
 #List already existing single bond connections
-bonds<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("bond atomRefs",
+bonds<-readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt")[grep("bond atomRefs.+order=\\\"1\\\"",
     readLines("C:\\Users\\Matthew S. MacLennan\\Desktop\\New Text Document.txt"),perl=T)]
 bondlist<-unlist(lapply(strsplit(bonds,"\\\""),function(x) x[2]))
 #Number bonds?
+bondlistidmat<-matrix(unlist(strsplit(bondlist," ")),ncol=2,byrow=T)
+nonHatomId<-unlist(lapply(strsplit(nonHatom,"\\\""),function(x) x[2]))
+nonHatomIdBondList<-bondlistidmat[bondlistidmat[,1] %in% nonHatomId & bondlistidmat[,2] %in% nonHatomId,]
+
+
 #create string for new conections
 
 
