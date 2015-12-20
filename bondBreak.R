172499e7243232f9bb63a60fae130e5717e3aaf4
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-12-19 17:03:32 -0800

    Update bondBreak.R

--- bondBreak.R
+++ bondBreak.R
@@ -5,19 +5,25 @@
 #Reload new file into R
 bondbreak<-function(smiles,generations){
 list1<-list()
-  for(i in 1:generations){
-    smiles<-AllOriginatorsList(unlist(smiles))
-    list1[[i]]<-smiles
-  }
-list1
-write.table(paste0(unlist(list1),collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
+for(j in 1:length(smiles)){
+  genlist<-list()
+    for(i in 1:generations){
+      smiles<-AllOriginatorsList(unlist(smiles))
+      genlist[[i]]<-smiles
+    }
+  
+
+genlist
+write.table(paste0(unlist(genlist),collapse="\n"),"sn.smi",col.names=F,row.names=F,quote=F)
 indirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.smi"
 outdirfile<-"C:/Users/Matthew S. MacLennan/Documents/sn.out2"
 #substituting indirfile with the path as character string returns error
 #system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -h")
 system2("cmd.exe",input="babel -ismi \"C:/Users/Matthew S. MacLennan/Documents/sn.smi\" -osmi \"C:/Users/Matthew S. MacLennan/Documents/sn.out2\" --separate -xc -xh -h")
-new<-read.table(outdirfile)
+list1[[j]]<-read.table(outdirfile)
 
+}
+list1
 }
 
 #add explicit H to smiles
